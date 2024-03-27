65f7ce53aa3428edcfcc8fba34ad639347280818
atiarrahaman <mowuj92@gmail.com>
2024-03-28 00:31:03 +0600

    setting

--- point_of_sale/settings.py
+++ point_of_sale/settings.py
@@ -10,13 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-^@9ejsq0$1g1a$84q-6)=x&(fq9nosd=jjhfnd_v_r#06+qlpo'
+SECRET_KEY = 'django-insecure-^@9ejsq0$1g1a$84q-6)=x&(fq9nosd=jjhfnd_v_r#06+qlpo'
 CSRF_TRUSTED_ORIGINS = [
     'https://sale-management.onrender.com', 'https://*.127.0.0.1']
 
 env = environ.Env()
 environ.Env.read_env()
-SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
