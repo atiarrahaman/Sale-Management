432fc433eac03df56640c6ccec349a687bc53ee8
atiarrahaman <mowuj92@gmail.com>
2024-07-03 21:16:27 +0600

    updated staff view

--- core/models.py
+++ core/models.py
@@ -10,7 +10,6 @@ class ShopOwner(models.Model):
     tex_id = models.CharField(max_length=200)
     shop_location = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
-
     def __str__(self):
         return f'{self.shop_name}'
 
--- inventory/views.py
+++ inventory/views.py
@@ -369,3 +369,4 @@ class ReturnToSupplierProductListView(TemplateView):
 
         return context
 
+
