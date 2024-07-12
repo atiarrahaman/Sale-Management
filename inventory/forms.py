7cccb5db03d8185de47066e868f7d80c5f18a105
atiarrahaman <mowuj92@gmail.com>
2024-07-12 16:23:17 +0600

    Update Product Quantity

--- inventory/forms.py
+++ inventory/forms.py
@@ -5,7 +5,25 @@ from .models import Inventory, Category, Supplier, ReturnToSupplier,Brand
 from product.models import Product
 
 
+class QuantityForm(forms.Form):
+   product = forms.ModelChoiceField(
+       queryset=Product.objects.all(),
+       empty_label='Select Product',
+       label='',
+       widget=forms.Select(attrs={
+           'class': 'form-control',
+       })
+   )
+   quantity = forms.IntegerField(
+       label='',
+       widget=forms.NumberInput(attrs={
+           'class': 'form-control',
+           'placeholder': 'Enter Quantity'
+       })
+   )
+
 class BrandForm(forms.ModelForm):
+
     name = forms.CharField(widget=forms.TextInput(
         attrs={'class': 'form-control'}))
     class Meta:
